39ccd4c0e9644695c31e28a2503b670ce6a0930a
rajaraman04 <rajaramansiva667@gmail.com>
2026-03-03 21:41:45 -0500

    Rectified a bug

--- src/app_temp.jsx
+++ src/app_temp.jsx
@@ -1,7 +1,7 @@
 // src/app/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Compare from "./pages/compare";
+import Compare from "./pages/Compare";
 
 export default function App() {
   return (
